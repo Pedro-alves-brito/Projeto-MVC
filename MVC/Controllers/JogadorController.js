15a303fd8742eaa19d4e9f0f715be96caa1a2a06
Pedro-alves-brito <pedrobrito0313@gmail.com>
2025-04-25 14:01:32 -0300

    Add files via upload

--- MVC/Controllers/JogadorController.js
+++ MVC/Controllers/JogadorController.js
@@ -3,7 +3,7 @@ const jogador = require("../Models/JogadorModel")
 class jogadorController{
     constructor(app){
         app.get("/jogador/criar", (req, res)=>{
-            res.send("Jogador Criado")
+            res.send("<h1>Jogador Criado</h1>")
         })
     }
 }
